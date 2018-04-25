63b979b75ec9fafdfaa4f31d7209df86c7dcf533
Michael Spiering <mspiering@pivotal.io>
2018-04-25 18:26:40 -0400

    Add print of environment variables, and give a simple example of how we could switch tasks based on comamnd from the PCF scheduler

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
-
+import java.util.Map;
 import java.time.Instant;
+import org.springframework.boot.DefaultApplicationArguments;
 
 @Log
 @EnableTask
@@ -15,11 +16,35 @@ import java.time.Instant;
 public class DemoApplication {
 	
 	@Bean
-	ApplicationRunner run() {
+	    ApplicationRunner run(DefaultApplicationArguments args2) {
+
+
+	    System.out.println("\nRead All Variables:-\n");
+ 
+	    Map <String, String> map = System.getenv();
+	    for (Map.Entry <String, String> entry: map.entrySet()) {
+		System.out.println("Variable Name:- " + entry.getKey() + " Value:- " + entry.getValue());
+	    }
+
+
+	    for (String arg3 : args2.getSourceArgs()) {
+		log.info(arg3);
+	    
+		if(arg3.compareTo("endpoint1")==0)
+		   {
+		       log.info("Should call endpoint 1");
+		   }
+		if(arg3.compareTo("endpoint2")==0)
+		      {
+			  log.info("should call endpoint 2");
+		      }
+	    
+	    }
 		return args -> log.info("running at " + Instant.now().toString());
 	}
 
 	public static void main(String[] args) {
+	    log.info("test");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
